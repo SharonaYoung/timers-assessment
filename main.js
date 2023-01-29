2318dcfc1abdcbbc30f1f6c61d7c522c2023725a
Sharona Young <msrona806@gmail.com>
2023-01-29 01:18:30 -0500

    button event listener working

---
+++ main.js
@@ -0,0 +1,15 @@
+// target button element
+const buttonEl = document.querySelector('button');
+buttonEl.addEventListener('click', () => {
+  console.log("button clicked");
+} )
+
+// timer setup
+
+// choose a random letter
+
+// record user keyboard event
+
+// if statement for if user selects correct letter
+
+// image popup after 20s
