5361460a8d4cc00a5e7b86761b85a3b654f045d8
Sharona Young <msrona806@gmail.com>
2023-01-29 19:55:46 -0500

    added code for timer

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-// target button element
+// target html elements
+const body = document.querySelector('body');
 const buttonEl = document.querySelector('button');
-buttonEl.addEventListener('click', () => {
-  console.log("button clicked");
-} )
+const h1El = document.querySelector('h1');
+
 
 // timer setup
+let timer = 10;
+buttonEl.addEventListener('click', () => {
+  console.log("button clicked");
+  let intId = setInterval(() => {  
+    h1El.textContent = `Time remaining: ${timer}`;
+    if(timer === 0){
+      clearInterval(intId);
+      const timeUp = document.createElement('p');
+      timeUp.innerText = "Time's up, you lost";
+      document.body.appendChild(timeUp);
+    }
+    timer--;
+  }, 1000)
+});
+
 
 // choose a random letter
 
